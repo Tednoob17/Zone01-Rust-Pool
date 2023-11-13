13dcb1dec3ec5657c7b8ba87339e219a13b7410c
Ted Kouhouenou <teddams047@gmail.com>
2023-11-13 12:42:49 +0100

    Scalar types

---
+++ Quest01-rust/0x02-scalar/target/debug/deps/scalar-96fdadb9d4325873.d
@@ -0,0 +1,5 @@
+/home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/0x02-scalar/target/debug/deps/scalar-96fdadb9d4325873: src/main.rs
+
+/home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/0x02-scalar/target/debug/deps/scalar-96fdadb9d4325873.d: src/main.rs
+
+src/main.rs:
---
+++ Quest01-rust/0x02-scalar/target/debug/scalar.d
@@ -0,0 +1,1 @@
+/home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/0x02-scalar/target/debug/scalar: /home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/0x02-scalar/src/main.rs
