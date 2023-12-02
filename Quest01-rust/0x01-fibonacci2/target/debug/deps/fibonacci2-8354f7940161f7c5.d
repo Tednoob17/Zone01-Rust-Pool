184781ade2ac32a2612adb3b15c5c3d62454a25e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-02 12:27:17 +0100

    Check & build fibonacci

--- Quest01-rust/0x01-fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.d
+++ Quest01-rust/0x01-fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.d
@@ -1,5 +1,5 @@
-/home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.rmeta: src/main.rs
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x01-fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.rmeta: src/main.rs
 
-/home/j3kyll/SCOP/01-Rust-Pool/Quest01-rust/fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.d: src/main.rs
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x01-fibonacci2/target/debug/deps/fibonacci2-8354f7940161f7c5.d: src/main.rs
 
 src/main.rs:
---
+++ Quest01-rust/0x01-fibonacci2/target/release/fibonacci2.d
@@ -0,0 +1,1 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x01-fibonacci2/target/release/fibonacci2: /home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x01-fibonacci2/src/main.rs
