ca36ae2c9707ead5f42a7141c476c19fda43733c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-02 23:37:59 +0100

    Check & build temp_conv

--- Quest01-rust/0x03-temperature_conv/src/main.rs
+++ Quest01-rust/0x03-temperature_conv/src/main.rs
@@ -1,3 +1,16 @@
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0/9.0
+}
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0/5.0) + 32.0    
+}
+
+//use temperature_conv::*;
+
+
 fn main() {
-    println!("Hello, world!");
+	println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
+	println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
 }
---
+++ Quest01-rust/0x03-temperature_conv/target/debug/temperature_conv.d
@@ -0,0 +1,1 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x03-temperature_conv/target/debug/temperature_conv: /home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x03-temperature_conv/src/main.rs
