6a0754b949ca82c792dfa2aa66ff6cb6d1d39efd
Ted Kouhouenou <teddams047@gmail.com>
2023-12-04 09:56:39 +0100

    Looping check

--- Quest01-rust/0x04-looping/src/main.rs
+++ Quest01-rust/0x04-looping/src/main.rs
@@ -3,14 +3,17 @@ use std::io;
 fn main() {
    let mut i = 0;
    let mut guess = String::new();
-   while guess =! 'e'  {
 
+   loop {
    println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place.\
    	        What am I?");
    io::stdin().read_line(&mut guess)
    	.expect("Failed to read the line");
-   i = i +1;
+   i = i + 1;
+   if guess == "e" {
+      break;
+   }
 };
-   println!("Number of trials: {}",i);
+   println!("Number of trials: {}", i);
 
 }
---
+++ Quest01-rust/0x04-looping/target/debug/deps/looping-ac7c53094ec01099.d
@@ -0,0 +1,5 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x04-looping/target/debug/deps/looping-ac7c53094ec01099: src/main.rs
+
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x04-looping/target/debug/deps/looping-ac7c53094ec01099.d: src/main.rs
+
+src/main.rs:
