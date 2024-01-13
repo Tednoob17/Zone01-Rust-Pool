7ca13301558f568b97cbd94eb4aa6fd625031dd8
Ted Kouhouenou <teddams047@gmail.com>
2024-01-13 17:16:06 +0100

    fibonacci

--- Quest01-rust/0x04-looping/src/main.rs
+++ Quest01-rust/0x04-looping/src/main.rs
@@ -1,20 +1,23 @@
 use std::io;
 
 fn main() {
-   let mut i = 0;
-   let mut guess = String::new();
+   let mut answer = String::new();
+   let mut count = 0;
+   let save = "The letter e";
 
    loop {
-   
-   println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place.\
-   	        What am I?");
-   io::stdin().read_line(&mut guess)
-   	.expect("Failed to read the line");
-   i = i + 1;
-   if guess  {
-      break;
-   }
-};
-   println!("Number of trials: {}", i);
 
+   println!("I am the beginning of the end, and the end of time and space. \
+   	       I am essential to creation, and I surround every place. What am I? :");
+   count +=1;
+   io::stdin()
+	.read_line(&mut answer)
+	.expect("Failed to read line");
+
+   if answer == "e" {
+	break;
+     }
+  };
+
+   println!("Number of trials: {count}");
 }
