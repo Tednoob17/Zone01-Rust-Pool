bb8287e6a7716d889f036ae96a99c6b801087dc4
Ted Kouhouenou <teddams047@gmail.com>
2024-01-13 21:37:55 +0100

    retest looping

--- Quest01-rust/0x04-looping/src/main.rs
+++ Quest01-rust/0x04-looping/src/main.rs
@@ -1,23 +1,22 @@
 use std::io;
 
-fn main() {
-   let mut answer = String::new();
+fn main() -> io::Result<()> {
    let mut count = 0;
    let save = "The letter e";
-
+   
    loop {
-
+   let mut answer = String::new();
    println!("I am the beginning of the end, and the end of time and space. \
    	       I am essential to creation, and I surround every place. What am I? :");
    count +=1;
-   io::stdin()
-	.read_line(&mut answer)
-	.expect("Failed to read line");
+   io::stdin().read_line(&mut answer).unwrap();	
 
-   if answer == "e" {
+   if answer.trim() == save  {
 	break;
      }
   };
 
    println!("Number of trials: {count}");
+   Ok(())
 }
+
