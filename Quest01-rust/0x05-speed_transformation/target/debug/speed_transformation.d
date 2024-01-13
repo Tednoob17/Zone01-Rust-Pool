b913e674c51c1f3fde80b821c5a98d58bba9d006
Ted Kouhouenou <teddams047@gmail.com>
2024-01-13 17:35:08 +0100

    speed transformation

---
+++ Quest01-rust/0x05-speed_transformation/target/debug/speed_transformation.d
@@ -0,0 +1,1 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x05-speed_transformation/target/debug/speed_transformation: /home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x05-speed_transformation/src/main.rs
