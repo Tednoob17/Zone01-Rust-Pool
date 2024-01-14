8edaf40b7c1da7edac4a1ad2c41d41e0a016088e
Ted Kouhouenou <teddams047@gmail.com>
2024-01-14 13:53:51 +0100

    global

---
+++ Quest01-rust/0x07-reverse-strings/src/main.rs
@@ -0,0 +1,18 @@
+
+
+pub fn rev_str(input: &str) -> String {
+	
+}
+
+
+
+//use reverse_string::rev_str;
+
+fn main() {
+    println!("{}", rev_str("Hello, world!"));
+    println!("{}", rev_str("Hello, my name is Roman"));
+    println!("{}", rev_str("I have a nice car!"));
+    println!("{}", rev_str("How old are You"));
+    println!("{}", rev_str("ex: this is an example água"));
+}
+
---
+++ Quest01-rust/0x07-reverse-strings/target/debug/deps/everse_strings-a9225f83cb1e0db1.d
@@ -0,0 +1,5 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x07-reverse-strings/target/debug/deps/everse_strings-a9225f83cb1e0db1: src/main.rs
+
+/home/j3kyll/SCOP/Zone01-Rust-Pool/Quest01-rust/0x07-reverse-strings/target/debug/deps/everse_strings-a9225f83cb1e0db1.d: src/main.rs
+
+src/main.rs:
