8abb65aa51d2f5757d22b44bcfac68c94025e5c9
Ted Kouhouenou <teddams047@gmail.com>
2024-01-16 22:54:21 +0100

    pass div_and_rem

---
+++ Quest01-rust/0xA-division_and_remainder/division_and_remainder.rs
@@ -0,0 +1,5 @@
+
+
+pub fn divide(x: i32, y: i32) -> (i32, i32) {
+	(x/y, x%y)
+}
---
+++ Quest01-rust/0xA-division_and_remainder/main.rs
@@ -0,0 +1,14 @@
+
+
+mod division_and_remainder;
+use division_and_remainder::divide;
+
+fn main() {
+    let x = 9;
+    let y = 4;
+    let (division, remainder) = divide(x, y);
+    println!(
+        "{}/{}: division = {}, remainder = {}",
+        x, y, division, remainder
+    );
+}
