2e22a7d479452f1e6130bc628c15f11109110243
Ted Kouhouenou <teddams047@gmail.com>
2024-01-18 09:03:43 +0100

    check it

--- Quest01-rust/0x09-matrix_transposition/matrix_transposition.rs
+++ Quest01-rust/0x09-matrix_transposition/matrix_transposition.rs
@@ -1,6 +1,10 @@
 
-struct Matrix((i32, i32), (i32, i32));
+
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-	
+	let ((a, b), (c, d)) = m.0;
+	let transposed = Matrix((a, c), (b, d));
+
+	transposed	
 }
