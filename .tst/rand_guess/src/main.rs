7b79c0e07cb0a5a0a7d23e268967ba4160fef0b6
Ted Kouhouenou <teddams047@gmail.com>
2024-01-21 00:09:54 +0100

    crates rand

---
+++ .tst/rand_guess/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main(){
+	let mut choice = String::new();	
+	//choice = "Jojo".to_string();
+
+	println!("Guess the number");
+	println!("Plese enter the number");
+
+	
+	io::stdin()
+		.read_line(&mut choice)
+		.expect("Failed to read line");
+
+	println!("Your number is: {}", choice);
+}
+
