8b553db09646ae7c837efa3b362ec202cd3dae61
Ted Kouhouenou <teddams047@gmail.com>
2024-01-21 15:37:01 +0100

    second test

---
+++ .tst/very_guessing/target/debug/deps/very_guessing-c3bfd4557e38fbfe.d
@@ -0,0 +1,5 @@
+/home/j3kyll/SCOP/Zone01-Rust-Pool/.tst/very_guessing/target/debug/deps/very_guessing-c3bfd4557e38fbfe: src/main.rs
+
+/home/j3kyll/SCOP/Zone01-Rust-Pool/.tst/very_guessing/target/debug/deps/very_guessing-c3bfd4557e38fbfe.d: src/main.rs
+
+src/main.rs:
